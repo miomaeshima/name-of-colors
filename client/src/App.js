d48484770c722fcd1af51a3e3fa83b4a1a0d2538
miomaeshima <miomamomo@gmail.com>
2021-04-19 14:51:04 +0900

    deleted Router as the design of the website is changed into a sinple scrollable page

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,6 @@ import SelectFile from "./components/Selectfile/SelectFile.js";
 
 function App() {
   return (
-    // <BrowserRouter>
-    // <Switch>
-    // <Route path="/" component = {Home} exact></Route>
-    // <Route path="/selectfile" component = {SelectFile}></Route>
-    // </Switch>
-    // </BrowserRouter>
     <Fragment>
       <Home/>
       <SelectFile />
--- client/src/components/utility.js
+++ client/src/components/utility.js
@@ -1,21 +1,6 @@
 import ColorThief from "colorthief";
 const colorThief = new ColorThief();
 
-//function to send rgb value to get the name of closest color from server
-
-// const getName = (rgbValue) =>{    
-//   try {
-//     fetch(`http://localhost:5000/color/${rgbValue}`)
-//       .then((res) => res.json())
-//       .then((color)=>{
-//         console.log(color)
-//         return 100
-//       })
-//     } catch (err) {
-//     console.log("getName is not working!");
-//   }
-// };
-
 const getName = async (rgbValue) =>{
   let res =  await fetch(`http://localhost:5000/color/${rgbValue}`)
   const color = await res.json();
@@ -31,17 +16,6 @@ const getRgb = async (e) => {
     
     let rgb = {r: result[0], g: result[1], b: result[2]};
     let rgbToBeSent = JSON.stringify(rgb);    
-    
-    // const promise = getName(rgbToBeSent);
-    // promise
-    // .then ((color)=>{
-    //   return color
-    // }
-    // )
-    //   .catch((e)=>console.error(e));
-    //   } else {
-    //   console.log("pic is not completed yet")
-
     let color = await getName(rgbToBeSent);
     return color
   }
