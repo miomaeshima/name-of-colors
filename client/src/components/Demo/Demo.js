51f72b3e4725ec842f66765ecf85bb0c548b0262
miomaeshima <miomamomo@gmail.com>
2021-04-22 16:56:09 +0900

    designed how the color name is shown for demo. Edited Demo component.

--- client/src/components/Demo/Demo.js
+++ client/src/components/Demo/Demo.js
@@ -1,18 +1,45 @@
-import {useState, Fragment} from "react";
+import { useState, Fragment } from "react";
 import stones from "../images/stones.jpg";
-import {getRgb} from "../utility.js";
+import { getRgb } from "../utility.js";
 import "./Demo.css";
 
+const Demo = () => {
+  const [clicked, setClicked] = useState(false);
+  const [color, setColor] = useState({});
 
-function Demo (){
-    return (
+  const getColor = async (e) => {
+    // e.preventDefault();
+    setClicked(true);
+
+    let data = await getRgb(e);
+    setColor(data);
+  };
+
+  return (
     <Fragment>
-    <div id="photoContainer">
-      <img id="sampleImage" alt="beige stone pebbles" onClick={getRgb} src={stones}/>
-      <div id="demoText">写真をクリックすると、この写真で一番使われている色の名前が分かります。</div>      
+      <div
+        id="demoContainer"
+        style={{ background: `rgb(${color.r}, ${color.g}, ${color.b})` }}
+      >
+        <div id="photoContainer">
+          <img
+            id="sampleImage"
+            alt="beige stone pebbles"
+            onClick={getColor}
+            src={stones}
+          />
+          {!clicked ? (
+            <div id="demoText">
+              写真をクリックすると、この写真で一番使われている色の名前が分かります。
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div id="demoNameBox">{color.name}</div>
       </div>
     </Fragment>
-    )
-}
+  );
+};
 
 export default Demo;
