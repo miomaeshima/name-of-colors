186de936a0a5e1a60cefec92d224259998e3fe86
miomaeshima <miomamomo@gmail.com>
2021-04-22 13:01:28 +0900

    separated demo portion to another component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 //import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {Fragment} from "react";
+import { Fragment } from "react";
 import "./App.css";
 import Home from "./components/Home/Home.js";
+import Demo from "./components/Demo/Demo.js";
 import SelectFile from "./components/Selectfile/SelectFile.js";
-
 
 function App() {
   return (
     <Fragment>
-      <Home/>
+      <Home />
+      <Demo />
       <SelectFile />
     </Fragment>
   );
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,6 @@
 //import {Link} from "react-router-dom";
 import "./Home.css";
-import stones from "../images/stones.jpg";
+// import stones from "../images/stones.jpg";
 import {getRgb} from "../utility.js"
 // import ColorThief from "colorthief";
 // const colorThief = new ColorThief();
@@ -12,11 +12,24 @@ function Home() {
       <div id="intro">
         <div id="title">色の名前</div>
         <div id="description">
-          ロレム・イプサムの嘆き、トマト大好き学部のエリット、しかし時と活力、そのような労働と悲しみ、ブラインド行うにはいくつかの重要な事柄に座ります。長年にわたり、私は学区と長寿であれば、そのような刺激の取り組み、彼女のうち、運動の利点を分注を邪魔されたする人が来ます。クピダタットのつるの痛みになりたい宿題に、批判されてきたら痛み、マグナ逃亡しても結果の喜びを生成しません。先例クピダタットブラックは先例していない、つまり、彼らはあなたの悩みに責任がある人の、一般的な義務を捨て、魂を癒しています。ご自身のデバイスの中の画像を選ぶには<a href="#selectyourfile">アップロードページ</a>をクリックしてください。
-          {/* <Link to="/selectfile">アップロードページ</Link>をクリックしてください。 */}
+        
+        例えば、色の名前を知りたい。青でもなく空色でもなく、「白藍　しらあい」という名前が知りたい、というときに使ってください。<br/><br/>
+
+        最終的には画像内の好きな箇所の色が選べるようになりますが、このバージョンでは、選んだ画像で一番使われている色の名前が分かります。<br/><br/>
+
+        画像を選んでも、ネット上で送られるのは一番使われている色のRGB値だけ。画像自体はどこにも送られません。<br/><br/>
+        <div id="clickContainer"><div id="selectYourFile"><a href="#selectyourfile" class="button" id="button1">自分のデバイス内の画像を調べる。</a> 
+        {/* <Link to="/selectfile">アップロードページ</Link>をクリックしてください。 */}
+        </div>
+        <div id="demo"><a href="#sampleImage" class="button">デモを見る。</a>
+        </div></div>              
         </div>
       </div>
-      <div id="photoContainer"><img className="sampleimage" alt="beige stone pebbles" onClick={getRgb} src={stones}/></div>
+      {/* <div id="photoContainer">
+      <img className="sampleimage" alt="beige stone pebbles" onClick={getRgb} src={stones}/>
+      <div id="demoText">写真をクリックすると、この写真で一番使われている色の名前が分かります。</div>      
+      </div> */}
+      
     </div>
   );
 }
