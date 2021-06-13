0008a907a3f78b3a409adeeadcbc6bf66642c120
miomaeshima <miomamomo@gmail.com>
2021-06-14 00:07:10 +0900

    added global style on App.css, edited Home for layout

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css";
 
+
 function Home() {
   return (
     <div id="home">
@@ -7,26 +8,19 @@ function Home() {
         <div id="title">色の名前</div>
         <div id="description">
           例えば、青でもなく空色でもなく「白藍
-          しらあい」という、日本の伝統色の名前が知りたいとき使えます。
-          <br />
-          <br />
-          最終的には画像内の好きな箇所の色が選べることを目指しますが、このバージョンでは、選んだ画像で一番使われている色の名前を調べます。
-          <br />
-          <br />
-          画像を選んでも、ネット上で送られるのは一番使われている色のRGB値だけ。画像自体はどこにも送られません。
-          <br />
-          <br />
-          <div id="clickContainer">
-            <div>
+          しらあい」という、日本の伝統色の名前が知りたいとき使えます。         最終的には画像内の好きな箇所の色が選べることを目指しますが、このバージョンでは、選んだ画像で一番使われている色の名前を調べます。
+          画像を選んでも、ネット上で送られるのは一番使われている色のRGB値だけ。画像自体はどこにも送られません。                   
+        </div>
+        <div id="clickContainer">
+          <div>
               <a href="#selectYourFile" className="button" id="button1">
                 自分のデバイス内の画像を調べる。
               </a>
-            </div>
-            <div>
+          </div>
+          <div>
               <a href="#demoImage" className="button">
                 デモを見る。
               </a>
-            </div>
           </div>
         </div>
       </div>
