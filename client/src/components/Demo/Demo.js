68c398d8037357dec37de9d254d625ba3eb08929
miomaeshima <miomamomo@gmail.com>
2021-06-13 22:32:42 +0900

    edited Demo.js to place the page to show icons when image is clicked

--- client/src/components/Demo/Demo.js
+++ client/src/components/Demo/Demo.js
@@ -8,10 +8,15 @@ const Demo = () => {
   const [colorData, setColorData] = useState({});
   const [demoColor, setDemoColor] = useState("lightgoldenrodyellow");
 
+  const placeDemoPage= ()=>{
+    window.location.href = "#demoImage"
+  }
+
   const getColor = async (e) => {
     let data = await getRgb(e);
     setColorData(data);
     setDemoColor(`rgb(${data.r}, ${data.g}, ${data.b})`);
+    placeDemoPage();
   };
 
   const refresh = () => {
@@ -38,6 +43,7 @@ const Demo = () => {
     <Fragment>
       <div id="demoContainer" style={{ background: demoColor }}>
         <div id="photoContainer">
+        
           <img
             id="demoImage"
             alt="beige stone pebbles"
@@ -46,6 +52,7 @@ const Demo = () => {
             style={clickable ? { cursor: "pointer" } : { cursor: "revert" }}
             tabIndex="0"
           />
+          
           {clickable ? (
             <div id="demoText">
               写真をクリックすると、この写真で一番使われている色の名前が分かります。
