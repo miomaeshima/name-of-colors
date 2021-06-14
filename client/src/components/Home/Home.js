a4ce15db663cb02c0da61e9168f8a3d2cc32d2d3
miomaeshima <miomamomo@gmail.com>
2021-06-14 11:50:42 +0900

    editing Home.js and Home.css for responsive design

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,35 +1,29 @@
 import "./Home.css";
 
-
 function Home() {
   return (
     <div id="home">
       <div id="wrapper">
         <div id="title">色の名前</div>
-      </div> 
+      </div>
       <div id="wrapper">
-        
-          <div id="intro">
-            例えば、青でもなく空色でもなく「白藍
-            しらあい」という、日本の伝統色の名前が知りたいとき使えます。         最終的には画像内の好きな箇所の色が選べることを目指しますが、このバージョンでは、選んだ画像で一番使われている色の名前を調べます。
-            画像を選んでも、ネット上で送られるのは一番使われている色のRGB値だけ。画像自体はどこにも送られません。                   
-          </div>
-          <div id="clickContainer">
-            <div>
-                <a href="#selectYourFile" className="button" id="button1">
-                  自分のデバイス内の画像を調べる。
-                </a>
-            </div>
-            <div>
-                <a href="#demoImage" className="button">
-                  デモを見る。
-                </a>
-            </div>
-          </div>
+        <div id="intro">
+          例えば、青でもなく空色でもなく「白藍
+          しらあい」という、日本の伝統色の名前が知りたいとき使えます。
+          最終的には画像内の好きな箇所の色が選べることを目指しますが、このバージョンでは、選んだ画像で一番使われている色の名前を調べます。
+          画像を選んでも、ネット上で送られるのは一番使われている色のRGB値だけ。画像自体はどこにも送られません。
+        </div>
+        <div id="clickContainer">
+          <a href="#selectYourFile" className="link" id="button1">
+            自分のデバイス内の画像を調べる。
+          </a>
+          <a href="#demoImage" className="link">
+            デモを見る。
+          </a>
         </div>
       </div>
-  
- );
+    </div>
+  );
 }
 
 export default Home;
